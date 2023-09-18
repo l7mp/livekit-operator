b57dcdd1ce446c8ed9a91c837bb45e3b550b1cc2
Kornel David <konel1020@gmail.com>
2023-09-18 11:37:45 +0200

    Initial commit with first version livekit operator crd

---
+++ api/v1alpha1/livekitoperator_types.go
@@ -0,0 +1,182 @@
+/*
+Copyright 2023 Kornel David.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+type TURNServer struct {
+	Host       string `json:"host"`
+	Port       int    `json:"port"`
+	Protocol   string `json:"protocol"`
+	Username   string `json:"username,omitempty"`
+	Credential string `json:"credential,omitempty"`
+}
+
+type RTCConfig struct {
+	PortRangeStart *int32 `json:"port_range_start"`
+	PortRangeEnd   *int32 `json:"port_range_end"`
+	TcpPort        *int32 `json:"tcp_port"`
+
+	// TURNServers holds the configuration for the user defined
+	// TURN servers. In case users want to define themselves this they can.
+	// However, it is advised to omit this to let the operator configure it.
+	// +optional
+	TURNServers *[]TURNServer `json:"turn_servers,omitempty"`
+}
+
+type Config struct {
+	Keys     *map[string]string `json:"keys"`
+	LogLevel *string            `json:"log_level"`
+	Port     *int32             `json:"port"`
+	Redis    *map[string]string `json:"redis"`
+	RTC      RTCConfig          `json:"rtc"`
+}
+
+type Deployment struct {
+
+	// Number of desired pods. This is a pointer to distinguish between explicit zero and not
+	// specified. Defaults to 1.
+	//
+	// +optional
+	Replicas *int32 `json:"replicas,omitempty"`
+
+	// Container template for the containers created in each Pod in the replicaset.
+	// If omitted the default template will be used. Which spawns a single container
+	//TODO
+	//
+	// +optional
+	Container v1.Container `json:"container"`
+
+	// Config holds the configuration for the livekit server that is executed.
+	//
+	//
+	Config *Config `json:"config"`
+}
+
+type MediaServer struct {
+
+	// +kubebuilder:validation:Enum=livekit
+	// +kubebuilder:default=livekit
+	// +optional
+	Type string `json:"type,omitempty"`
+
+	// +kubebuilder:validation:Required
+	Deployment *Deployment `json:"deployment"`
+}
+
+// Ingress is the LiveKit tool not the gateway resource to ingest traffic into the cluster
+type Ingress struct {
+	//TODO
+}
+
+type Egress struct {
+	//TODO
+}
+
+type CertManager struct {
+	//TODO
+}
+
+type Monitoring struct {
+	//TODO
+}
+
+type Component struct {
+
+	// MediaServer is the main resource that the operator manages. By default, it supports
+	// only the LiveKit server as a media server but in the future it might support other
+	// media servers as well.
+	// +kubebuilder:validation:Required
+	MediaServer *MediaServer `json:"mediaServer"`
+
+	// LiveKit's Ingress resource descriptor.
+	// This resource makes it possible to stream videos(prerecorded or live) into
+	// the Kubernetes cluster and further into a chosen room. Note that this resource
+	// enables a one-way communication between the client and the media server.
+	//
+	// +optional
+	Ingress *Ingress `json:"ingress,omitempty"`
+
+	// LiveKit's Egress resource descriptor.
+	// Egress makes it possible to stream a single user's or any number of users'
+	// streams out of a room onto an RTMP port.
+	//
+	// +optional
+	Egress *Egress `json:"egress,omitempty"`
+
+	// CertManager manages the cert
+	//TODO
+	//
+	//
+	// +optional
+	CertManager *CertManager `json:"certManager,omitempty"`
+
+	// Monitoring enables the Prometheus metric exposition, installs
+	// a Prometheus operator and Grafana operator with the corresponding resources
+	//
+	// +optional
+	Monitoring *Monitoring `json:"monitoring,omitempty"`
+}
+
+// LiveKitOperatorSpec defines the desired state of LiveKitOperator
+type LiveKitOperatorSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of LiveKitOperator. Edit livekitoperator_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+
+	// +kubebuilder:validation:Required
+	Components *Component `json:"components"`
+}
+
+// LiveKitOperatorStatus defines the observed state of LiveKitOperator
+type LiveKitOperatorStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// LiveKitOperator is the Schema for the livekitoperators API
+type LiveKitOperator struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   LiveKitOperatorSpec   `json:"spec,omitempty"`
+	Status LiveKitOperatorStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// LiveKitOperatorList contains a list of LiveKitOperator
+type LiveKitOperatorList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []LiveKitOperator `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&LiveKitOperator{}, &LiveKitOperatorList{})
+}
---
+++ go.mod
@@ -0,0 +1,101 @@
+module github.com/l7mp/livekit-operator
+
+go 1.19
+
+require (
+	github.com/livekit/livekit-server v1.4.5
+	github.com/onsi/ginkgo/v2 v2.6.0
+	github.com/onsi/gomega v1.24.1
+	k8s.io/api v0.26.0
+	k8s.io/apimachinery v0.26.0
+	k8s.io/client-go v0.26.0
+	sigs.k8s.io/controller-runtime v0.14.1
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
+	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/zapr v1.2.3 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-openapi/swag v0.19.14 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/gnostic v0.5.7-v3refs // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/livekit/mediatransportutil v0.0.0-20230815100155-96164dbcfd8c // indirect
+	github.com/livekit/protocol v1.6.1 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pion/datachannel v1.5.5 // indirect
+	github.com/pion/dtls/v2 v2.2.7 // indirect
+	github.com/pion/ice/v2 v2.3.10 // indirect
+	github.com/pion/interceptor v0.1.17 // indirect
+	github.com/pion/logging v0.2.2 // indirect
+	github.com/pion/mdns v0.0.7 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/pion/rtcp v1.2.10 // indirect
+	github.com/pion/rtp v1.8.1 // indirect
+	github.com/pion/sctp v1.8.7 // indirect
+	github.com/pion/sdp/v3 v3.0.6 // indirect
+	github.com/pion/srtp/v2 v2.0.16 // indirect
+	github.com/pion/stun v0.6.1 // indirect
+	github.com/pion/transport/v2 v2.2.1 // indirect
+	github.com/pion/turn/v2 v2.1.3 // indirect
+	github.com/pion/webrtc/v3 v3.2.16 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.16.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.10.1 // indirect
+	github.com/redis/go-redis/v9 v9.1.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/urfave/cli/v2 v2.25.7 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.25.0 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/oauth2 v0.5.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/term v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/apiextensions-apiserver v0.26.0 // indirect
+	k8s.io/component-base v0.26.0 // indirect
+	k8s.io/klog/v2 v2.80.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20221012153701-172d655c2280 // indirect
+	k8s.io/utils v0.0.0-20221128185143-99ec85e7a448 // indirect
+	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+)
