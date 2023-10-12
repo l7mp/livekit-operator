8d520202b74318dba32c1cb5ebf125548ef37c2b
Kornel David <konel1020@gmail.com>
2023-10-12 12:26:24 +0200

    dev: Update livekitmesh types and crd
    Add lkMesh sample CR and and sample configmap

--- api/v1alpha1/livekitmesh_types.go
+++ api/v1alpha1/livekitmesh_types.go
@@ -189,9 +189,6 @@ type LiveKitMeshSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of LiveKitMesh. Edit livekitmesh_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
-
 	// +kubebuilder:validation:Required
 	Components *Component `json:"components"`
 }
