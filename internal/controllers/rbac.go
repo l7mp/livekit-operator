6da7b20ec77884c96f5432898541497b1ecb7831
Kornel David <konel1020@gmail.com>
2024-05-17 01:01:22 +0200

    fix: Broken helm install and missing resources

--- internal/controllers/rbac.go
+++ internal/controllers/rbac.go
@@ -7,7 +7,15 @@ package controllers
 
 // RBAC for references in watched resources.
 // +kubebuilder:rbac:groups=core,resources=serviceaccounts;services;secrets;configmaps;,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=roles;rolebindings,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps,resources=deployments;statefulsets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles;clusterrolebindings,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=gateway.networking.k8s.io,resources=gateways;gatewayclasses;tcproutes;httproutes,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=stunner.l7mp.io,resources=udproutes;gatewayconfigs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=cert-manager.io,resources=issuers,verbs=get;list;watch;create;update;patch;delete
+
+// Helm chart related RBAC
+// +kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=get;list;watch;create;update;patch
+// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles;clusterrolebindings,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=stunner.l7mp.io,resources=dataplanes,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=admissionregistration.k8s.io,resources=mutatingwebhookconfigurations,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=batch.k8s.io,resources=jobs,verbs=get;list;watch;create;update;patch;delete
